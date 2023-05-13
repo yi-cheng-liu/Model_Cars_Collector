e0025e58b21b24936e65e38d3349d1ffe51702fd
yi-cheng-liu <liuyiche@umich.edu>
2023-05-13 16:27:06 -0400

    finish home page

--- main/models.py
+++ main/models.py
@@ -71,6 +71,7 @@ class Diecast(models.Model):
     buying_price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     retail_price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     on_sale = models.BooleanField(default=False)
+    on_sale_home = models.BooleanField(default=False)
     on_sale_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank = True, help_text="ON SALE: on sale price < retail price")
     production_year = models.IntegerField(default=datetime.now().year,
             validators=[MinValueValidator(1800), MaxValueValidator(datetime.now().year)])    
