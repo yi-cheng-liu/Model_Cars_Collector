b14f0a650bd5057db141ae7bc6d323818b3c6b87
yi-cheng-liu <liuyiche@umich.edu>
2023-06-27 18:37:06 -0400

    add vercel config

--- model_car_collector/wsgi.py
+++ model_car_collector/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "model_car_collector.settings")
 
 application = get_wsgi_application()
+
+app = application
